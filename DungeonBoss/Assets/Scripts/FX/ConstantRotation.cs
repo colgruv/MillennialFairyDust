b8b6636126b4cbacae98e3bfb8bbebf78532e5bd
BabyMaybe <Eric_Varela@mac.com>
2015-08-23 16:23:54 -0400

    Hexes
    Yeah, mostly hexes

---
+++ DungeonBoss/Assets/Scripts/FX/ConstantRotation.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ConstantRotation : MonoBehaviour {
+
+	public float x,y,z;
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Rotate(x,y,z);
+	}
+}
---
+++ DungeonBoss/Assets/Scripts/FX/TextureScroll.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TextureScroll : MonoBehaviour {
+
+	public float x,y;
+
+	Renderer r;
+
+	// Use this for initialization
+	void Start () {
+		r = GetComponent<Renderer>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float offsetX = Time.time * x;
+		float offsetY = Time.time * y;
+		r.material.SetTextureOffset("_MainTex", new Vector2(offsetX,offsetY));
+	}
+}
