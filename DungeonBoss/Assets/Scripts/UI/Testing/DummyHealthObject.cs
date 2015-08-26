7e3b4c2362bb0d835eff39cff8f051d99a310a43
BabyMaybe <Eric_Varela@mac.com>
2015-08-26 01:02:23 -0400

    Health Bar Better
    Added graphics for info bar and added health bar functionality

---
+++ DungeonBoss/Assets/Scripts/UI/Testing/DummyHealthObject.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class DummyHealthObject : MonoBehaviour {
+
+	public Slider visualizer;
+	public Slider healthBar;
+
+	public float currentHealth;
+	public float totalHealth;
+	private float percentHealth;
+
+	public Text healthDisplay;
+	public Text healthTotalDisplay;
+
+	public Text minusText;
+	public Text plusText;
+
+	// Use this for initialization
+	void Start () {
+
+		healthTotalDisplay.text = totalHealth.ToString();
+		healthDisplay.text = currentHealth.ToString();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	private void DisplayText()
+	{
+		healthDisplay.text = currentHealth.ToString();
+	}
+	
+	private void UpdateHealth(float delta)
+	{
+		currentHealth = Mathf.Clamp(currentHealth + delta,0,totalHealth);
+		DisplayText();
+		percentHealth = currentHealth/totalHealth;
+		visualizer.value = percentHealth;
+		healthBar.value = percentHealth;
+	}
+
+	public void IncreaseHealth(float delta)
+	{
+		float randomized =  (int)Random.Range(delta * -1, delta) + delta;
+		UpdateHealth(randomized);
+		plusText.text = randomized.ToString();
+	}
+
+	public void DecreaseHealth(float delta)
+	{
+		float randomized =  (int)Random.Range(delta * -1, delta) + delta;
+		UpdateHealth(randomized);
+		minusText.text = randomized.ToString();
+	}
+
+}
