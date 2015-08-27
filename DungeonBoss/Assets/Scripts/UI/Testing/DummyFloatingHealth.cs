28d31c98e1952617e06ac7d35ba71b9bd1b331fc
BabyMaybe <Eric_Varela@mac.com>
2015-08-26 21:52:11 -0400

    Floating Health Bars
    Functional Dummy Health Bars For Adventurers

---
+++ DungeonBoss/Assets/Scripts/UI/Testing/DummyFloatingHealth.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class DummyFloatingHealth : MonoBehaviour {
+
+	public Slider floatingHealth;
+	public float totalHealth;
+	public float currentHealth;
+	private float percentHealth;
+
+	// Use this for initialization
+	void Start () {
+		percentHealth = currentHealth/totalHealth;
+		floatingHealth.value = percentHealth;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnMouseDown()
+	{
+
+		currentHealth += Random.Range(-15,0);
+		if (currentHealth <= 0f)
+		{
+			Destroy(transform.parent.gameObject);
+		}
+
+		percentHealth = currentHealth/totalHealth;
+		floatingHealth.value = percentHealth;
+
+	}
+}
