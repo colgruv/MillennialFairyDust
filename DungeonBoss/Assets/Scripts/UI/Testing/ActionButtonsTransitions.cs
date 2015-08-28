7d41aabe7e9b48d5bea9214b27af6902d674907b
BabyMaybe <Eric_Varela@mac.com>
2015-08-27 22:18:20 -0400

    Action Menu Proof of Concept
    Basic Structure of Action Menu Implemented

---
+++ DungeonBoss/Assets/Scripts/UI/Testing/ActionButtonsTransitions.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ActionButtonsTransitions : MonoBehaviour {
+
+	public Animator transitions;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void ToggleHidden()
+	{
+		transitions.SetBool("hidden", !transitions.GetBool("hidden"));
+	}
+
+	public void ToggleSpells()
+	{
+		transitions.SetBool("spells", !transitions.GetBool("spells"));
+	}
+}
